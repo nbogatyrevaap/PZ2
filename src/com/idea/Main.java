4306eb7630d905afbfa1ecaaf968228344fa191c
Natalia Bogatyreva <nerudblok@mail.ru>
2022-04-19 17:34:56 +0300

    PZ2

---
+++ src/com/idea/Main.java
@@ -0,0 +1,15 @@
+package com.idea;
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        float a  = (float) (3.4*1038f);
+        byte b = (byte) (-128);
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
